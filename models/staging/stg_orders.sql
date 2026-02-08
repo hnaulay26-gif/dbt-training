ff1ec5e75add5ef36e6a78c415dfe1bd67d363ce
naulayhimanshu26@gmail.com <naulayhimanshu26@gmail.com>
2026-02-08 23:01:31 +0000

    Developing stg_orders

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,23 @@
+select 
+--from raw_order
+o.orderId,
+o.orderdate,
+o.shipdate,
+o.shipmode,
+        ordersellingprice-ordercostprice AS OrderProfit,
+        o.ordercostprice,
+        o.ordersellingprice,
+--from raw_cusstomer
+c.customername,
+c.segment,
+c.country,
+--from raw_product
+p.category,
+p.productname,
+p.subcategory
+FROM
+    {{ ref('raw_order') }} as o
+LEFT JOIN {{ ref('raw_customer') }} as c
+ON o.customerid=c.customerid
+LEFT JOIN {{ ref('raw_product') }} as p
+ON o.productid=p.productid
